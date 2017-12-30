3d87a2b3ddd849416f4e8dd15e08da09250c131b
Owaix <aowais37@gmail.com>
2017-12-30 11:29:22 -0500

    Add files via upload

---
+++ DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using EHRMS.IdentityExtensions;
+using DataAccess;
+
+namespace EHRMS.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET Dashboard
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        //public async Task<IActionResult> Indexer()
+        //{
+        //    ApplicationUser user  = await 
+        //    return View();
+        //}
+        public ActionResult Tabs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Indexr()
+        {
+            HrContext db = new HrContext();
+            return View(db.menu.ToList());
+        }
+    }
+}
