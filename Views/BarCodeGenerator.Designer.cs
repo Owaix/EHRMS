342676932ec76fc3c392059b28363fd0a83b8551
Owaix <aowais37@gmail.com>
2018-03-05 06:38:04 -0500

    Add files via upload

---
+++ Views/BarCodeGenerator.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Barcode
+{
+    partial class BarCodeGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.generateBarCodeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.generateBarCodeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(108, 62);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(23, 36);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(249, 20);
+            this.txtCode.TabIndex = 1;
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DS";
+            reportDataSource1.Value = this.generateBarCodeBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Barcode.Reports.Barcode.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(-4, 112);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(288, 219);
+            this.reportViewer1.TabIndex = 2;
+            // 
+            // generateBarCodeBindingSource
+            // 
+            this.generateBarCodeBindingSource.DataSource = typeof(Barcode.generateBarCode);
+            // 
+            // BarCodeGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 332);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "BarCodeGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BarCodeGenerator";
+            this.Load += new System.EventHandler(this.BarCodeGenerator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.generateBarCodeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtCode;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource generateBarCodeBindingSource;
+    }
+}
+
---
+++ Views/BarCodeGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zen.Barcode;
+
+namespace Barcode
+{
+    public partial class BarCodeGenerator : Form
+    {
+        public BarCodeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string[] data = txtCode.Text.Split(',');
+            generateBarCode obj = new generateBarCode
+            {
+                labNo = data[0],
+                mrNo = data[1],
+                patientName = data[2],
+                userName = data[3]
+            };
+            GenerateBarCode(obj.labNo);
+            txtCode.Text = "";
+            MessageBox.Show("BarCode Successfully Generated.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            obj.barCode = $"{Application.StartupPath}\\Barcode\\{obj.labNo}.png";
+            reportViewer1.LocalReport.EnableExternalImages = true;
+            reportViewer1.LocalReport.SetParameters(new Microsoft.Reporting.WinForms.ReportParameter("barCode", "file:" + obj.barCode));
+            generateBarCodeBindingSource.DataSource = obj;
+            this.reportViewer1.RefreshReport();
+        }
+        private void GenerateBarCode(string labNo)
+        {
+            Code128BarcodeDraw zbc = BarcodeDrawFactory.Code128WithChecksum;
+            System.Drawing.Image img2 = zbc.Draw(labNo, 20, 1);
+            System.IO.MemoryStream ms2 = new System.IO.MemoryStream();
+            img2.Save(ms2, System.Drawing.Imaging.ImageFormat.Png);
+            System.Windows.Forms.PictureBox pb = new PictureBox();
+            pb.Image = img2;
+            pb.Image.Save(Application.StartupPath + "\\Barcode\\" + labNo.Replace("/", "").Replace("\\", "") + ".png");
+        }
+
+        private void BarCodeGenerator_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+    public class generateBarCode
+    {
+        public String labNo { get; set; } = "";
+        public String mrNo { get; set; } = "";
+        public String patientName { get; set; } = "";
+        public String userName { get; set; } = "";
+        public String barCode { get; set; } = "";
+    }
+}
